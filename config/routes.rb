e9a989dd1d8cac3d1e3673c7e25fb6baa698f6c3
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-02 22:19:45 +0200

    Adding about page and bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  match '/about', to: 'about#index', :via => 'get' 
   root 'home#index'
-
   # get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
